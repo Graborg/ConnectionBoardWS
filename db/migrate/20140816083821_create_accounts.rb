15760e11377cebe34c6fc016028cad68701bd121
Mikael Gråborg <intemicke@gmail.com>
2014-10-17 00:16:09 +0200

    for science

--- db/migrate/20140816083821_create_accounts.rb
+++ db/migrate/20140816083821_create_accounts.rb
@@ -1,12 +1,10 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
-  	unless table_exists? :accounts
 	    create_table :accounts do |t|
 	      t.string :password_digest
 	      t.string :username
 
 	      t.timestamps
-	  	end
     end
   end
 end
