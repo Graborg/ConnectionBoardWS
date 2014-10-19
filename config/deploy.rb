c0611e2225b67ed6e995eb3aef254f813ec23fff
Mikael Gråborg <intemicke@gmail.com>
2014-10-19 21:28:51 +0200

    Nginx now reloading instead of restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute "sudo /etc/init.d/nginx restart"
+      execute "sudo /etc/init.d/nginx reload"
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
     end
